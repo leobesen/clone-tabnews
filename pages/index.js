7ec818b704d5d26a98090a71eee5025f6536681b
Leonardo Besen <leobesen@hotmail.com>
2026-02-21 19:58:32 -0300

    update Home component to include project details and author information

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 function Home() {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of the application.</p>
+      <h1>Welcome to the Home Page of Clone Tabnews</h1>
+      <p>This is the main landing page of the application created for the curso.dev.</p>
+      <p>Author: leobesen</p>
     </div>
   );
 }
