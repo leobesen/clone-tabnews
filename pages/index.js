422b463b7b060344376243301441e49d2851b06a
Leonardo Besen <leobesen@hotmail.com>
2026-02-22 19:48:07 -0300

    Fix author description into index page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
     <div>
       <h1>Welcome to the Home Page of Clone Tabnews</h1>
       <p>This is the main landing page of the application created for the curso.dev.</p>
-      <p>Author: leobesen</p>
+      <p>Author: leo besen</p>
     </div>
   );
 }
