484438041dd93e9e397bb7edf998f2b861251911
Leonardo Besen <leobesen@hotmail.com>
2026-02-26 19:51:05 -0300

    format README and index.js for improved readability

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page of Clone Tabnews</h1>
-      <p>This is the main landing page of the application created for the curso.dev.</p>
+      <p>
+        This is the main landing page of the application created for the
+        curso.dev.
+      </p>
       <p>Author: leo besen</p>
     </div>
   );
