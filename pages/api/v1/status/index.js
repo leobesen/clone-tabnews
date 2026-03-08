1c57874d219536d044321f7ce3161d09a1d16aad
Leonardo Besen <leobesen@hotmail.com>
2026-03-08 23:19:13 +0000

    implement database query in status API endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from '../../../../infra/database.js';
+
+async function status(request, response) {
+  const res = await database.query('SELECT 1 + 1;');
+  console.log('Database response: ', res)
   response.status(200).json({
     status: 'ok',
     timestamp: new Date().toISOString(),
