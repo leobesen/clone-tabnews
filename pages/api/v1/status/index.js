b4c28509ab5d616a065f4d915ec81316bc7c68cf
Leonardo Besen <leobesen@hotmail.com>
2026-03-12 23:54:15 +0000

    [#6] refactor: update status API to accept a database name parameter for opened connections

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,7 +4,10 @@ async function status(request, response) {
   const updatedAt = new Date().toISOString();
   const pgVersion = await database.getPGVersion();
   const pgMaxConnections = await database.getPGMaxConnections();
-  const pgCurrentUsedConnections = await database.getPGOpenedConnections();
+
+  const databaseName = request.query.database || process.env.POSTGRES_DB;
+
+  const pgCurrentUsedConnections = await database.getPGOpenedConnections(databaseName);
 
   console.log("PostgreSQL Version:", pgVersion);
   console.log("PostgreSQL Max Connections:", pgMaxConnections);
