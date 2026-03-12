41f9115b88b7220837a33b55cf2f85a0a3f5658f
Leonardo Besen <leobesen@hotmail.com>
2026-03-12 08:55:01 +0000

    [#6] refactor: replace Client with Pool for database connections and add utility functions

--- infra/database.js
+++ infra/database.js
@@ -1,17 +1,36 @@
-import { Client } from "pg";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
-  const res = await client.query(queryObject);
-  await client.end();
+  const res = await pool.query(queryObject);
   return res;
 }
 
-export default { query };
+async function getPGVersion() {
+  const res = await query("SHOW server_version;");
+  return res.rows[0].server_version;
+}
+
+async function getPGMaxConnections() {
+  const res = await query("SHOW max_connections;");
+  return res.rows[0].max_connections;
+}
+
+async function getPGCurrentUsedConnections() {
+  const res = await query("SELECT count(*) FROM pg_stat_activity;");
+  return res.rows[0].count;
+}
+
+export default {
+  query,
+  getPGVersion,
+  getPGMaxConnections,
+  getPGCurrentUsedConnections,
+};
