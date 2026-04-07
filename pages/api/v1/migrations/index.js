3a1f39ccbae6ebd8940ed89e4eaed69970bab65e
Leonardo Besen <leobesen@hotmail.com>
2026-04-07 00:20:57 +0000

    [#7] feat: refactor migration handler to improve request method handling and configuration

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,15 +2,31 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 async function migrations(request, response) {
-  const migrations = await migrationRunner({
+  const defaultMigrationsConfig = {
     databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
+    dryRun: false,
     dir: join("infra", "migrations"),
     direction: "up",
     verbose: true,
     migrationsTable: "pgmigrations",
-  });
-  response.status(200).json(migrations);
+  };
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner(defaultMigrationsConfig);
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    } else {
+      return response.status(200).json([]);
+    }
+  }
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationsConfig,
+      dryRun: true,
+    });
+    return response.status(200).json(pendingMigrations);
+  }
+
+  return response.status(405).json({ error: "Method not allowed" });
 }
 
 export default migrations;
