6ce9e6cf4b6ef4170ff260eadf020ad978f37238
Leonardo Besen <leobesen@hotmail.com>
2026-04-07 00:19:53 +0000

    feat: add Jest configuration and update test scripts for improved execution

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config({
+  path: path.resolve(__dirname, ".env.development"),
+});
+
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+const jestConfig = createJestConfig({
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+});
+
+module.exports = jestConfig;
